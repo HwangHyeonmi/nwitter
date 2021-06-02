a939d71550356ce10b8ae802fdb509a1d6b7b9e5
HwangHyeonmi <49122297+HwangHyeonmi@users.noreply.github.com>
2021-06-02 17:41:10 +0900

    프로필 수정중

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -10,9 +10,11 @@ const Profile = ({userObj}) => {
         history.push("/");
     }
     const getMyNweets = async() =>{
+        //누구인지 알려줘야 프로필 얻을 수 있음.
         const nweets = await dbService
         .collection("nweets")
         .where("creatorId", "==", userObj.uid)
+        .orderBy("createdAt")
         .get();
         console.log(nweets.docs.map(doc =>doc.data()));
     }
